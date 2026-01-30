c0c26f67759e7d85947df44b3f1d3a78585176ea
RuslanSenani <abbasovruslan214@gmail.com>
2026-01-30 16:36:24 +0400

    Home Page Bir hissesi hazirlandi
    (Media, Contact, Vakansia) modullarinda duzelisler olunacaq

--- resources/views/Front/Partners_v/content.blade.php
+++ resources/views/Front/Partners_v/content.blade.php
@@ -1,3 +1,53 @@
+<!-- Page Banner -->
+<div class="page-banner about-banner container-fluid no-padding">
+    <!-- Container -->
+    <div class="container">
+        <h3>{{$siteContent['home_partners']->value ?? 'Parnyorlar'}}</h3>
+
+        <ol class="breadcrumb">
+            <li><a href="{{route('home')}}">{{$siteContent['home_home']->value??''}}</a></li>
+            <li class="active">{{$siteContent['home_partners']->value??''}}</li>
+        </ol>
+    </div><!-- Container /- -->
+    <!-- Shape -->
+    <div class="banner-shape container-fluid no-padding">
+        <div class="col-md-6 col-sm-6 col-xs-6 shape-left no-padding">
+            <div class="left-shape-blue">
+                <svg width="100%" height="100%">
+                    <clipPath id="clipPolygon2" clipPathUnits="objectBoundingBox">
+                        <polygon points="0 0, 0 100, 120 100, 0 0"></polygon>
+                    </clipPath>
+                </svg>
+            </div>
+            <div class="left-shape">
+                <svg width="100%" height="100%">
+                    <clipPath id="clipPolygon1" clipPathUnits="objectBoundingBox">
+                        <polygon points="0 0, 0 100, 100 100, 0 0"></polygon>
+                    </clipPath>
+                </svg>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6 shape-right no-padding">
+            <div class="right-shape-blue">
+                <svg width="100%" height="100%">
+                    <clipPath id="clipPolygon3" clipPathUnits="objectBoundingBox">
+                        <polygon points="1 0.2, 0 1, 0 0.835, 1 0"></polygon>
+                    </clipPath>
+                </svg>
+            </div>
+            <div class="right-shape">
+                <svg width="100%" height="100%">
+                    <clipPath id="clipPolygon4" clipPathUnits="objectBoundingBox">
+                        <polygon points="1 0, 0 1, 100 100, 100 0"></polygon>
+                    </clipPath>
+                </svg>
+            </div>
+        </div>
+    </div><!-- Shape -->
+</div><!-- Page Banner /- -->
+
+
+
 
 <div id="what-we-do-best" class="container-fluid no-padding what-we-do-best">
     <!-- What We Do Best Left -->
