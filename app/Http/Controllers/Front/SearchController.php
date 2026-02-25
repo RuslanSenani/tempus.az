8735ed5071282c8071a7ae80724a8c881bb663b0
RuslanSenani <abbasovruslan214@gmail.com>
2026-02-26 00:48:05 +0400

    live-search bug fix

--- app/Http/Controllers/Front/SearchController.php
+++ app/Http/Controllers/Front/SearchController.php
@@ -16,45 +16,45 @@ class SearchController extends Controller
     {
         $this->siteContent = $siteContent;
     }
+//
+//    public function liveSearch(Request $request): JsonResponse
+//    {
+//        // \DB::enableQueryLog();
+//        $query = $request->get('query');
+//
+//        if (!$query || mb_strlen($query) < 2) {
+//            return response()->json([]);
+//        }
+//
+////        $currentLang = app()->getLocale() ?: 'az';
+//
+//        $results = Preparation::query()
+//            ->where(function ($q) use ($query) {
+//                $q->where('name->az', 'LIKE', "%{$query}%")
+//                    ->orWhere('title->az', 'LIKE', "%{$query}%")
+//                    ->orWhere('slug', 'LIKE', "%{$query}%");
+//            })
+//            ->take(5)
+//            ->get(['id', 'name', 'title']);
+//// \DB::getQueryLog()
+//        return response()->json($results);
+//
+//    }
+
 
     public function liveSearch(Request $request): JsonResponse
     {
-        // \DB::enableQueryLog();
+        $siteContent = $this->siteContent->getAllContent();
+
         $query = $request->get('query');
 
-        if (!$query || mb_strlen($query) < 2) {
-            return response()->json([]);
-        }
-
-//        $currentLang = app()->getLocale() ?: 'az';
-
-        $results = Preparation::query()
-            ->where(function ($q) use ($query) {
-                $q->where('name->az', 'LIKE', "%{$query}%")
-                    ->orWhere('title->az', 'LIKE', "%{$query}%")
-                    ->orWhere('slug', 'LIKE', "%{$query}%");
-            })
+        $results = Preparation::where('name', 'LIKE', "%{$query}%")
+            ->orWhere('title', 'LIKE', "%{$query}%")
+            ->orWhere('slug', 'LIKE', "%{$query}%")
             ->take(5)
             ->get(['id', 'name', 'title']);
-// \DB::getQueryLog()
+
+
         return response()->json($results);
-
     }
-
-
-//    public function liveSearch(Request $request): JsonResponse
-//    {
-//        $siteContent = $this->siteContent->getAllContent();
-//
-//        $query = $request->get('query');
-//
-//        $results = Preparation::where('name', 'LIKE', "%{$query}%")
-//            ->orWhere('title', 'LIKE', "%{$query}%")
-//            ->orWhere('slug', 'LIKE', "%{$query}%")
-//            ->take(5)
-//            ->get(['id', 'name', 'title']);
-//
-//
-//        return response()->json($results);
-//    }
 }
