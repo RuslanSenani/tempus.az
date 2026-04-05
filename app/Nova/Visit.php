878eaa784eeb751a56b601999e171786bdf08d13
RuslanSenani <abbasovruslan214@gmail.com>
2026-04-05 18:04:49 +0400

    visit nova-ya reason sutun elave edildi

--- app/Nova/Visit.php
+++ app/Nova/Visit.php
@@ -56,17 +56,19 @@ class Visit extends Resource
             Text::make('IP Ünvanı', 'ip_address')
                 ->sortable()
                 ->copyable(),
-//            Badge::make('Səbəb', 'reason')
-//                ->map([
-//                    'DANGEROUS_PATH' => 'danger',
-//                    'FAKE_BROWSER_NO_OS' => 'warning',
-//                    'MALICIOUS_AGENT' => 'danger',
-//                    'RATE_LIMIT_EXCEEDED' => 'info',
-//                    'PROGRAMMATIC_BOT' => 'warning',
-//                ])
-//                ->withDefault('info') // Əgər siyahıda yoxdursa 'info' rəngini ver
-//                ->sortable()
-//                ->nullable(),
+            Badge::make('Səbəb', 'reason', function () {
+                // Əgər bazada NULL və ya boşdursa, 'CLEAN' qaytar, yoxsa özünü
+                return $this->reason ?: 'CLEAN';
+            })
+                ->map([
+                    'CLEAN' => 'success',  // Yaşıl (Real İnsanlar)
+                    'DANGEROUS_PATH' => 'danger',   // Qırmızı
+                    'FAKE_BROWSER_NO_OS' => 'warning',  // Narıncı
+                    'MALICIOUS_AGENT' => 'danger',   // Qırmızı
+                    'RATE_LIMIT_EXCEEDED' => 'info',     // Göy
+                    'PROGRAMMATIC_BOT' => 'warning',  // Narıncı
+                ])
+                ->sortable(),
 
             // Bot olub-olmadığını rəngli nişanla göstərək
             Badge::make('Status', 'is_bot')
