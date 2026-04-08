e7de5e4964f18b6911a11d429aa34711a38e693c
RuslanSenani <abbasovruslan214@gmail.com>
2026-04-09 00:47:42 +0400

    test numunesi silindi

--- app/Http/Middleware/LogVisits.php
+++ app/Http/Middleware/LogVisits.php
@@ -15,11 +15,6 @@ class LogVisits
         $ip = $request->ip();
         $userAgent = $request->userAgent() ?? '';
 
-        // 1. Whitelist - Özünü heç vaxt bloklama
-//        if (in_array($ip, ['127.0.0.1', '::1', '37.61.124.14'])) {
-//            return $next($request);
-//        }
-
         // 2. Cache-dən sürətli blok yoxlaması
         if (cache()->has('blocked_ip_' . $ip)) {
             $this->logAndAbort($request, $ip, 'RECURRING_BLOCK_TRY', false);
