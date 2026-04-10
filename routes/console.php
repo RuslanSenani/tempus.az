2afb25d772aeebe813fefb738a4301a8cd831621
RuslanSenani <abbasovruslan214@gmail.com>
2026-04-11 01:15:54 +0400

    bug fix

--- routes/console.php
+++ routes/console.php
@@ -16,22 +16,7 @@ Schedule::command('instagram:refresh-token')->monthlyOn(1, '03:00');
 Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
 
 Schedule::call(function () {
-    // 1. Bazaya gedən hər şeyi qulaq asırıq
-    DB::listen(function ($query) {
-        // Query-nin içində "visits" və "delete" sözü varsa log-a yaz
-        if (str_contains($query->sql, 'delete') && str_contains($query->sql, 'visits')) {
-            Log::error("Kankret SQL: " . $query->sql);
-            Log::error("Kankret Dəyərlər (Bindings): ", $query->bindings);
-        }
-    });
-
-    // 2. Əməliyyatı icra edirik
     Visit::where('created_at', '<', now()->subDays(1))->delete();
-
 })->everyMinute();
-
-//Schedule::call(function () {
-//    Visit::where('created_at', '<', now()->subDays(2))->delete();
-//})->daily();
 
 Schedule::command('app:generate-sitemap')->dailyAt('02:00');
