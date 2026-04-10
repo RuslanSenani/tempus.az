05ef32843ada462243ba2e4aded4d4a8629de3f5
RuslanSenani <abbasovruslan214@gmail.com>
2026-04-11 01:06:20 +0400

    bug fix

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use App\Models\Visit;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
@@ -14,7 +15,22 @@ Schedule::command('instagram:refresh-token')->monthlyOn(1, '03:00');
 Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
 
 Schedule::call(function () {
-    Visit::where('created_at', '<', now()->subDays(2))->delete();
-})->daily();
+    $query = Visit::where('created_at', '<', now()->subDays(2));
+
+    // Kankret olaraq sorğunu və dəyərləri götürürük
+    $sql = $query->toSql();
+    $bindings = $query->getBindings();
+
+    // Log-a birbaşa yazdırırıq
+    Log::info("SQL: " . $sql);
+    Log::info("Bindings: ", $bindings);
+
+    // Sonra da icra edirik
+    $query->delete();
+})->everyMinute();
+
+//Schedule::call(function () {
+//    Visit::where('created_at', '<', now()->subDays(2))->delete();
+//})->daily();
 
 Schedule::command('app:generate-sitemap')->dailyAt('02:00');
